e52ef3793e21ece72b42cde778b1b1c4c35bcb51
Pro100Serega01 <120277930+Pro100Serega01@users.noreply.github.com>
2024-10-03 13:37:28 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System.Text;
+using System.Text;
 
 namespace ЛАБ1_2курсС_
 {
@@ -18,7 +18,7 @@ namespace ЛАБ1_2курсС_
         }
         public bool isInRange(int a, int b, int num)
         {
-            return a <= num && num <= b;
+            return a <= num && num <= b || b <= num && num <= a;
         }
         public bool isEqual(int a, int b, int c)
         {
@@ -33,12 +33,10 @@ namespace ЛАБ1_2курсС_
         }
         public bool is35(int x)
         {
-            if (x % 3 == 0 || x % 5 == 0)
-                return true;
-            else if (x % 3 == 0 && x % 5 == 0)
+            if (x % 3 == 0 && x % 5 == 0)
                 return false;
             else
-                return false;
+                return true;
         }
         public int max3(int x, int y, int z)
         {
@@ -210,6 +208,7 @@ namespace ЛАБ1_2курсС_
                     Console.WriteLine("Введите ввещественное число:");
                     double x = float.Parse(Console.ReadLine());
                     x = program.fraction(x);
+                    Console.WriteLine("Ответ: ");
                     Console.WriteLine(x);
                     break;
                 case 2://МЕТОДЫ 3
@@ -217,6 +216,7 @@ namespace ЛАБ1_2курсС_
                     Console.WriteLine("Введите целое число от 0 до 9:");
                     char ch = char.Parse(Console.ReadLine());
                     int result = program.charToNum(ch);
+                    Console.WriteLine("Ответ: ");
                     Console.WriteLine(result);
                     break;
                 case 3://МЕТОДЫ 5
@@ -224,6 +224,7 @@ namespace ЛАБ1_2курсС_
                     Console.WriteLine("Введите проверяемое число: ");
                     int y = int.Parse(Console.ReadLine());
                     isWorking = program.is2Digits(y);
+                    Console.WriteLine("Ответ: ");
                     Console.WriteLine(isWorking);
                     break;
                 case 4://МЕТОДЫ 7
@@ -235,6 +236,7 @@ namespace ЛАБ1_2курсС_
                     Console.WriteLine("Введите число, которое хотите проверить:");
                     int number = int.Parse(Console.ReadLine());
                     isWorking = program.isInRange(a, b, number);
+                    Console.WriteLine("Ответ: ");
                     Console.WriteLine(isWorking);
                     break;
                 case 5://МЕТОДЫ 9
@@ -246,12 +248,15 @@ namespace ЛАБ1_2курсС_
                     Console.WriteLine("Введите число c:");
                     c = int.Parse(Console.ReadLine());
                     isWorking = program.isEqual(a, b, c);
+                    Console.WriteLine("Ответ: ");
+                    Console.WriteLine(isWorking);
                     break; 
                 case 6://УСЛОВИЯ 1
                     Console.WriteLine("Вы запустили задачу 1, раздел Условия");
                     Console.WriteLine("Введите число x: ");
                     x1 = int.Parse(Console.ReadLine());
                     x1 = program.abs(x1);
+                    Console.WriteLine("Ответ: ");
                     Console.WriteLine(x1);
                     break;
                 case 7://УСЛОВИЯ 3
@@ -259,6 +264,7 @@ namespace ЛАБ1_2курсС_
                     Console.WriteLine("Введите число x. которое делится на 3 или 5: ");
                     x1 = int.Parse(Console.ReadLine());
                     isWorking = program.is35(x1);
+                    Console.WriteLine("Ответ: ");
                     Console.WriteLine(isWorking);
                     break;
                 case 8://УСЛОВИЯ 5
@@ -270,6 +276,7 @@ namespace ЛАБ1_2курсС_
                     Console.WriteLine("Введите число z: ");
                     z1 = int.Parse(Console.ReadLine());
                     result =  program.max3(x1, y1, z1);
+                    Console.WriteLine("Ответ: ");
                     Console.WriteLine(result);
                     break;
                 case 9://УСЛОВИЯ 7
@@ -284,6 +291,7 @@ namespace ЛАБ1_2курсС_
                     Console.WriteLine("Введите число х: ");
                     x1 = int.Parse(Console.ReadLine());
                     str = program.day(x1);
+                    Console.WriteLine("Ответ: ");
                     Console.WriteLine(str);
                     break;
                 case 11://ЦИКЛЫ 1
@@ -291,6 +299,7 @@ namespace ЛАБ1_2курсС_
                     Console.WriteLine("Введите число х: ");
                     x1 = int.Parse(Console.ReadLine());
                     str = program.listNums(x1);
+                    Console.WriteLine("Ответ: ");
                     Console.WriteLine(str);
                     break; 
                 case 12://ЦИКЛЫ 3
@@ -298,6 +307,7 @@ namespace ЛАБ1_2курсС_
                     Console.WriteLine("Введите число х: ");
                     x1 = int.Parse(Console.ReadLine());
                     str = program.chet(x1);
+                    Console.WriteLine("Ответ: ");
                     Console.WriteLine(str);
                     break; 
                 case 13://ЦИКЛЫ 5
@@ -305,18 +315,21 @@ namespace ЛАБ1_2курсС_
                     Console.WriteLine("Введите число х: ");
                     x1 = int.Parse(Console.ReadLine());
                     x1 = program.numLen(x1);
+                    Console.WriteLine("Ответ: ");
                     Console.WriteLine(x1);
                     break; 
                 case 14://ЦИКЛЫ 7
                     Console.WriteLine("Вы запустили задачу 7, раздел Циклы");
                     Console.WriteLine("Введите число х: ");
                     x1 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Ответ: ");
                     program.square(x1);
                     break; 
                 case 15://ЦИКЛЫ 9
                     Console.WriteLine("Вы запустили задачу 9, раздел Циклы");
                     Console.WriteLine("Введите число х: ");
                     x1 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Ответ: ");
                     program.rightTriangle(x1);
                     break;
                 case 16://МАССИВЫ 1
@@ -327,6 +340,7 @@ namespace ЛАБ1_2курсС_
                     Console.WriteLine("\nВведите число х: ");
                     x1 = int.Parse(Console.ReadLine());
                     x1 = program.findFirst(arr, x1);
+                    Console.WriteLine("Ответ: ");
                     Console.WriteLine(x1);
                     break; 
                 case 17://МАССИВЫ 3
@@ -335,6 +349,7 @@ namespace ЛАБ1_2курсС_
                     for (int i = 0; i < arr.Length; i++)
                         Console.Write(arr[i] + " ");
                     x1 = program.maxAbs(arr);
+                    Console.WriteLine("Ответ: ");
                     Console.WriteLine(x1);
                     break;
                 case 18://МАССИВЫ 5
@@ -346,6 +361,7 @@ namespace ЛАБ1_2курсС_
                     Console.WriteLine("Введите число pos: ");
                     pos = int.Parse(Console.ReadLine());
                     arr = program.add(arr, arr2, pos);
+                    Console.WriteLine("Ответ: ");
                     for (int i = 0; i < arr.Length; i++)
                         Console.Write(arr[i] + " ");
                     break;
@@ -357,6 +373,7 @@ namespace ЛАБ1_2курсС_
                     Console.WriteLine();
                     Console.WriteLine("Тот же массив задом наперед: ");
                     arr = program.ReverseBack(arr);
+                    Console.WriteLine("Ответ: ");
                     for (int i = 0; i < arr.Length; i++)
                         Console.Write(arr[i] + " ");
                     break;
@@ -369,6 +386,7 @@ namespace ЛАБ1_2курсС_
                     Console.WriteLine("Введите число х: ");
                     x1 = int.Parse(Console.ReadLine());
                     arr = program.findAll(arr, x1);
+                    Console.WriteLine("Ответ: ");
                     for (int i = 0; i < arr.Length; i++)
                         Console.Write(arr[i] + " ");
                     break;
