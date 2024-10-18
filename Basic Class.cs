1df0d7b9d278880cce13d4cdf8cfb25ad3c31381
Pro100Serega01 <120277930+Pro100Serega01@users.noreply.github.com>
2024-10-18 10:49:17 +0300

    lab2_1

---
+++ Basic Class.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ЛАБ2_1
+{
+    public class Basic_Class
+    {
+        protected bool field1, field2;
+        public Basic_Class(bool field1, bool field2) 
+        {
+            this.field1 = field1;
+            this.field2 = field2;   
+        }
+        public Basic_Class(Basic_Class other)
+        {
+            this.field1 = other.field1;
+            this.field2 = other.field2;
+        }
+        public bool CalculateConjunction() 
+        { 
+            return field1 && field2; 
+        }
+        public override string ToString()
+        {
+            return $"field1 = {field1}, field2 = {field2}";
+        }
+    }
+}
---
+++ Dother_Class.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ЛАБ2_1
+{
+    class Dother_Class : Basic_Class
+    {
+        private string name;
+
+        // Конструктор
+        public Dother_Class(bool field1, bool field2, string name) : base(field1, field2)
+        {
+            this.name = name;
+        }
+
+        // Метод для получения информации о классе
+        public string GetInfo()
+        {
+            return $"{name}: {ToString()}";
+        }
+
+        // Метод проверки
+        public bool Check2field()
+        {
+            return field1 && field2;
+        }
+    }
+}
